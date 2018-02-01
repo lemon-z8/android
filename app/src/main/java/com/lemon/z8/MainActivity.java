64b7ec33254e10a82a7ebfddd1445f73cf069eca
Alx <120524148@qq.com>
2018-02-01 13:48:00 +0800

    test01 通过

---
+++ app/src/main/java/com/lemon/z8/MainActivity.java
@@ -0,0 +1,22 @@
+package com.lemon.z8;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
